65c30d5c925801280ce5d07b2f38d74ac4994b06
XinyiY5166 <xvy5166@psu.edu>
2020-09-10 13:45:11 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,53 @@
+# Author: Xinyi Yang xvy5166@psu.edu
+# Collaborator: Yiqian Li yvl5838@psu.edu
+# Collaborator: Harsh Ladani hal5240@psu.edu
+# Section: 4
+# Breakout: 14
+
+def getLetterGrade(grade):
+  if (grade >= 93.0):
+    return("Your letter grade for CMPSC 131 is A.")
+  elif (grade >= 90.0):
+    return("Your letter grade for CMPSC 131 is A-.")
+  elif (grade >= 87.0):
+    return("Your letter grade for CMPSC 131 is B+.")
+  elif (grade >= 83.0):
+    return("Your letter grade for CMPSC 131 is B.")
+  elif (grade >= 80.0):
+    return("Your letter grade for CMPSC 131 is B-.")
+  elif (grade >= 77.0):
+    return("Your letter grade for CMPSC 131 is C+.")
+  elif (grade >= 70.0):
+    return("Your letter grade for CMPSC 131 is C.")
+  elif (grade >= 60.0):
+    return("Your letter grade for CMPSC 131 is D.")
+  else:
+    return("Your letter grade for CMPSC 131 is F.")
+
+def run():
+  grade = float(input("Enter your CMPSC 131 grade: "))
+  if (grade >= 93.0):
+    print("Your letter grade for CMPSC 131 is A.")
+  elif (grade >= 90.0):
+    print("Your letter grade for CMPSC 131 is A-.")
+  elif (grade >= 87.0):
+    print("Your letter grade for CMPSC 131 is B+.")
+  elif (grade >= 83.0):
+    print("Your letter grade for CMPSC 131 is B.")
+  elif (grade >= 80.0):
+    print("Your letter grade for CMPSC 131 is B-.")
+  elif (grade >= 77.0):
+    print("Your letter grade for CMPSC 131 is C+.")
+  elif (grade >= 70.0):
+    print("Your letter grade for CMPSC 131 is C.")
+  elif (grade >= 60.0):
+    print("Your letter grade for CMPSC 131 is D.")
+  else:
+    print("Your letter grade for CMPSC 131 is F.")
+
+  
+
+if __name__ == "__main__":
+ run()
+  
+  
